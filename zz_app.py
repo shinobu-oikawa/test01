a7724e7645d7e5bbb207f50019b0ce76229029ac
shinobu-oikawa <67253639+shinobu-oikawa@users.noreply.github.com>
2023-06-02 08:53:57 +0900

    Update zz_app.py

--- zz_app.py
+++ zz_app.py
@@ -24,7 +24,7 @@ def generate_response(prompt):
         engine="GPTshinobu", # The 'engine' parameter specifies the name of the OpenAI GPT-3.5 Turbo engine to use.
         temperature=0.7, # The 'temperature' parameter controls the randomness of the response.
         max_tokens=4000, # The 'max_tokens' parameter controls the maximum number of tokens in the response.
-        top_p=0.95, # The 'top_p' parameter controls the diversity of the response.
+        top_p=0.92, # The 'top_p' parameter controls the diversity of the response.
         # The 'messages' parameter is set to the 'prompts' list to provide context for the AI model.
         messages = st.session_state['prompts']
     )
